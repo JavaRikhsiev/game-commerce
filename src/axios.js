1ef03daa651e02e289f1507363991cee6fab3d93
JavaRikhsiev <Javohir_99@inbox.ru>
2022-04-06 02:32:47 +0500

    The order history page (real time database) deployed

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: "http://localhost:5001/game-commerce-89714/us-central1/api",
+  baseURL: "https://us-central1-game-commerce-89714.cloudfunctions.net/api",
+  //"http://localhost:5001/game-commerce-89714/us-central1/api",
 });
 
 export default instance;
